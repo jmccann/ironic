5ca0f9334ef5e4ebcb9c8ebee3b4bcd6d423e364
Jacob McCann <jacob.mccann2@target.com>
2015-06-02 10:37:19 -0500

    The real fix

--- recipes/populate_neutron.rb
+++ recipes/populate_neutron.rb
@@ -1,19 +1,19 @@
 execute 'create private net' do
   command 'source ~/openrc && neutron net-create private'
-  not_if "source ~/openrc && neutron net-list -F name | egrep '\|[ ]+private[ ]+\|'"
+  not_if 'source ~/openrc && neutron net-list -F name | egrep "\|[ ]+private[ ]+\|"'
 end
 
 execute 'create private subnet' do
   command 'source ~/openrc && neutron subnet-create --name private-subnet private 10.1.0.0/24'
-  not_if "source ~/openrc && neutron subnet-list -F name | egrep '\|[ ]+private-subnet[ ]+\|'"
+  not_if 'source ~/openrc && neutron subnet-list -F name | egrep "\|[ ]+private-subnet[ ]+\|"'
 end
 
 execute 'create baremetal net' do
   command 'source ~/openrc && neutron net-create baremetal'
-  not_if "source ~/openrc && neutron net-list -F name | egrep '\|[ ]+baremetal[ ]+\|'"
+  not_if 'source ~/openrc && neutron net-list -F name | egrep "\|[ ]+baremetal[ ]+\|"'
 end
 
 execute 'create baremetal subnet' do
   command 'source ~/openrc && neutron subnet-create --name baremetal-subnet baremetal 192.168.50.0/24'
-  not_if "source ~/openrc && neutron subnet-list -F name | egrep '\|[ ]+baremetal-subnet[ ]+\|'"
+  not_if 'source ~/openrc && neutron subnet-list -F name | egrep "\|[ ]+baremetal-subnet[ ]+\|"'
 end
