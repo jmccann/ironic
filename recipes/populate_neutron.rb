aeea1db7e88e0c700e3ef6eaaf5f1fc9f2312a96
Jacob McCann <jacob.mccann2@target.com>
2015-06-02 10:12:12 -0500

    Fix all network population idempotency

--- recipes/populate_neutron.rb
+++ recipes/populate_neutron.rb
@@ -1,6 +1,6 @@
 execute 'create private net' do
   command 'source ~/openrc && neutron net-create private'
-  not_if "source ~/openrc && neutron net-list -F name | grep '| private |'"
+  not_if "source ~/openrc && neutron net-list -F name | egrep '\|[ ]+private[ ]+\|'"
 end
 
 execute 'create private subnet' do
@@ -10,10 +10,10 @@ end
 
 execute 'create baremetal net' do
   command 'source ~/openrc && neutron net-create baremetal'
-  not_if "source ~/openrc && neutron net-list -F name | grep '| baremetal |'"
+  not_if "source ~/openrc && neutron net-list -F name | egrep '\|[ ]+baremetal[ ]+\|'"
 end
 
 execute 'create baremetal subnet' do
-  command 'source ~/openrc && neutron subnet-create --name baremetal-subnet private 192.168.50.0/24'
-  not_if "source ~/openrc && neutron subnet-list -F name | grep '| baremetal-subnet |'"
+  command 'source ~/openrc && neutron subnet-create --name baremetal-subnet baremetal 192.168.50.0/24'
+  not_if "source ~/openrc && neutron subnet-list -F name | egrep '\|[ ]+baremetal-subnet[ ]+\|'"
 end
