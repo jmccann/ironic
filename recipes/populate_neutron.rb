1e2ded20e51708f9ae3a9dbbb65bbcb4bd2cb063
Jacob McCann <jacob.mccann2@target.com>
2015-06-06 10:02:36 -0500

    Got TFTP working

--- recipes/populate_neutron.rb
+++ recipes/populate_neutron.rb
@@ -8,6 +8,21 @@
 #   not_if 'source ~/openrc && neutron subnet-list -F name | egrep "\|[ ]+private-subnet[ ]+\|"'
 # end
 
+class ::Chef::Recipe # rubocop:disable Documentation
+  include ::Openstack
+end
+
+identity_endpoint = internal_endpoint 'identity-internal'
+
+# For glance client, only identity v2 is supported. See discussion on
+# https://bugs.launchpad.net/openstack-chef/+bug/1207504
+# So here auth_uri can not be transformed.
+auth_uri = identity_endpoint.to_s
+
+admin_user = node['openstack']['identity']['admin_user']
+admin_pass = get_password 'user', admin_user
+admin_tenant = node['openstack']['identity']['admin_tenant_name']
+
 execute 'ip link set enp0s8 up' do
   not_if 'ip link show enp0s8 | grep UP'
 end
@@ -17,11 +32,15 @@ execute 'ifconfig br-bare 192.168.50.1 netmask 255.255.255.0 up' do
 end
 
 execute 'create baremetal net' do
-  command 'source ~/openrc && neutron net-create baremetal --shared --provider:network_type flat --provider:physical_network physbare'
-  not_if 'source ~/openrc && neutron net-list -F name | egrep "\|[ ]+baremetal[ ]+\|"'
+    environment 'OS_USERNAME' => admin_user, 'OS_PASSWORD' => admin_pass,
+                'OS_TENANT_NAME' => admin_tenant, 'OS_AUTH_URL' => auth_uri
+  command 'neutron net-create baremetal --shared --provider:network_type flat --provider:physical_network physbare'
+  not_if 'neutron net-list -F name | egrep "\|[ ]+baremetal[ ]+\|"'
 end
 
 execute 'create baremetal subnet' do
-  command 'source ~/openrc && neutron subnet-create baremetal 192.168.50.0/24 --name baremetal-subnet --ip-version=4 --gateway=192.168.50.1 --allocation-pool start=192.168.50.100,end=192.168.50.200 --enable-dhcp'
-  not_if 'source ~/openrc && neutron subnet-list -F name | egrep "\|[ ]+baremetal-subnet[ ]+\|"'
+  environment 'OS_USERNAME' => admin_user, 'OS_PASSWORD' => admin_pass,
+              'OS_TENANT_NAME' => admin_tenant, 'OS_AUTH_URL' => auth_uri
+  command 'neutron subnet-create baremetal 192.168.50.0/24 --name baremetal-subnet --ip-version=4 --gateway=192.168.50.1 --allocation-pool start=192.168.50.100,end=192.168.50.200 --enable-dhcp'
+  not_if 'neutron subnet-list -F name | egrep "\|[ ]+baremetal-subnet[ ]+\|"'
 end
--- recipes/tftp.rb
+++ recipes/tftp.rb
@@ -1,9 +1,23 @@
-include_recipe 'tftp::server'
-
-# directory "#{node['tftp']['directory']}/pxelinux.cfg" do
-#   mode '0755'
-# end
-
 package 'syslinux-tftpboot'
 
-execute 'chmod -R 777 /var/lib/tftpboot'
+include_recipe 'tftp::server'
+
+# Override template to be able to specify flags
+r = resources('template[/etc/xinetd.d/tftp]')
+r.cookbook 'ironic'
+
+# Override tftpboot directory owner so ironic can write to it
+r = resources("directory[#{node['tftp']['directory']}]")
+r.owner 'ironic'
+r.group 'ironic'
+
+directory '/var/lib/tftpboot/var/lib' do
+  user 'ironic'
+  group 'ironic'
+  recursive true
+end
+
+execute 'ln -s ../.. tftpboot' do
+  cwd '/var/lib/tftpboot/var/lib'
+  not_if '[ -e /var/lib/tftpboot/var/lib/tftpboot ]'
+end
