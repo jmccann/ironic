e762d9815def75ae1323f09fc0baa96997ca96e2
Jacob McCann <jacob.mccann2@target.com>
2015-07-09 15:41:52 -0500

    Download discovery ramdisk only if missing

--- recipes/download_inspector_discovery.rb
+++ recipes/download_inspector_discovery.rb
@@ -1,7 +1,9 @@
 remote_file "#{node['ironic']['inspector']['tftpboot_path']}/discovery.kernel" do
   source node['ironic']['inspector']['discovery_kernel']
+  action :create_if_missing
 end
 
 remote_file "#{node['ironic']['inspector']['tftpboot_path']}/discovery.initramfs" do
   source node['ironic']['inspector']['discovery_initramfs']
+  action :create_if_missing
 end
