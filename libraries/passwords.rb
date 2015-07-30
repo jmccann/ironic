48e02d4a777967ffbbb377a75ec2cb78eca69e60
Jacob McCann <jacob.mccann2@target.com>
2015-07-30 14:11:24 -0500

    Include ChefVaultItem module

--- libraries/passwords.rb
+++ libraries/passwords.rb
@@ -1,6 +1,7 @@
 module ::Openstack
   # Contribute back to openstack-common
 
+  include ChefVaultItem
   def vault_secret(bag_name, index)
     ::Chef::Log.info "Loading vault secret #{index} from #{bag_name}"
     chef_vault_item(bag_name, index)[index]
