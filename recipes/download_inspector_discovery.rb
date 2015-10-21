9fe3999fcf17411e1647fef5fb962ab493a79e0a
Jacob McCann <jacob.mccann2@target.com>
2015-10-21 09:52:41 -0500

    Allow managing discovery ramdisk if checksum is provided

--- recipes/download_inspector_discovery.rb
+++ recipes/download_inspector_discovery.rb
@@ -1,9 +1,11 @@
 remote_file "#{node['openstack']['bare-metal']['tftp']['root_path']}/discovery.kernel" do
   source node['ironic']['inspector']['discovery_kernel']
-  action :create_if_missing
+  checksum node['ironic']['inspector']['discovery_kernel_checksum'] unless node['ironic']['inspector']['discovery_kernel_checksum'].nil?
+  action :create_if_missing if node['ironic']['inspector']['discovery_kernel_checksum'].nil?
 end
 
 remote_file "#{node['openstack']['bare-metal']['tftp']['root_path']}/discovery.initramfs" do
   source node['ironic']['inspector']['discovery_initramfs']
-  action :create_if_missing
+  checksum node['ironic']['inspector']['discovery_initramfs_checksum'] unless node['ironic']['inspector']['discovery_initramfs_checksum'].nil?
+  action :create_if_missing if node['ironic']['inspector']['discovery_kernel_checksum'].nil?
 end
--- spec/recipes/download_inspector_discovery_spec.rb
+++ spec/recipes/download_inspector_discovery_spec.rb
@@ -1,18 +1,39 @@
 require 'spec_helper.rb'
 
 describe 'ironic::download_inspector_discovery' do
-  cached(:chef_run) do
-    ChefSpec::SoloRunner.new(platform: 'redhat', version: '6.5') do |node|
-      node.set['ironic']['inspector']['discovery_kernel'] = 'https://test.com/discover.kernel'
-      node.set['ironic']['inspector']['discovery_initramfs'] = 'https://test.com/discover.initramfs'
-    end.converge(described_recipe)
+  context 'without checksum attribute values' do
+    cached(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'redhat', version: '6.5') do |node|
+        node.set['ironic']['inspector']['discovery_kernel'] = 'https://test.com/discover.kernel'
+        node.set['ironic']['inspector']['discovery_initramfs'] = 'https://test.com/discover.initramfs'
+      end.converge(described_recipe)
+    end
+
+    it 'downloads discovery kernel if it does not exist' do
+      expect(chef_run).to create_remote_file_if_missing('/var/lib/tftpboot/discovery.kernel').with(source: 'https://test.com/discover.kernel')
+    end
+
+    it 'downloads discovery initramfs if it does not exist' do
+      expect(chef_run).to create_remote_file_if_missing('/var/lib/tftpboot/discovery.initramfs').with(source: 'https://test.com/discover.initramfs')
+    end
   end
 
-  it 'downloads discovery kernel' do
-    expect(chef_run).to create_remote_file_if_missing('/var/lib/tftpboot/discovery.kernel').with(source: 'https://test.com/discover.kernel')
-  end
+  context 'with checksum attribute values' do
+    cached(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'redhat', version: '6.5') do |node|
+        node.set['ironic']['inspector']['discovery_kernel'] = 'https://test.com/discover.kernel'
+        node.set['ironic']['inspector']['discovery_initramfs'] = 'https://test.com/discover.initramfs'
+        node.set['ironic']['inspector']['discovery_kernel_checksum'] = 'abc123'
+        node.set['ironic']['inspector']['discovery_initramfs_checksum'] = 'abc1234'
+      end.converge(described_recipe)
+    end
 
-  it 'downloads discovery initramfs' do
-    expect(chef_run).to create_remote_file_if_missing('/var/lib/tftpboot/discovery.initramfs').with(source: 'https://test.com/discover.initramfs')
+    it 'downloads discovery kernel' do
+      expect(chef_run).to create_remote_file('/var/lib/tftpboot/discovery.kernel').with(source: 'https://test.com/discover.kernel', checksum: 'abc123')
+    end
+
+    it 'downloads discovery initramfs' do
+      expect(chef_run).to create_remote_file('/var/lib/tftpboot/discovery.initramfs').with(source: 'https://test.com/discover.initramfs', checksum: 'abc1234')
+    end
   end
 end
